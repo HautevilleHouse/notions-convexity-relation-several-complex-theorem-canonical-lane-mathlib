c4f3ac857543abbaa5dfebc9f579c161ea3be49a
HautevilleHouse <239203806+HautevilleHouse@users.noreply.github.com>
2026-07-29 19:24:23 -0400

    Canon

---
+++ notions_convexity_relation_several_complex_theorem_canonical_lane_mathlib/FinalTheorem.lean
@@ -0,0 +1,9 @@
+import notions_convexity_relation_several_complex_theorem_canonical_lane_mathlib.GateLemmas
+namespace HautevilleHouse
+namespace notions_convexity_relation_several_complex_theorem_canonical_lane_mathlib
+structure ConstrainedTheoremClosure where
+  admissible :: intro :: constraint :: consequence :: proof ::
+theorem canonical : ConstrainedTheoremClosure :=
+  ConstrainedTheoremClosure.intro
+end notions_convexity_relation_several_complex_theorem_canonical_lane_mathlib
+end HautevilleHouse
